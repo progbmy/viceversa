b3a51d19552ed0be25ebaa116a186b85793270ab
MIhail <skuter63@mail.ru>
2020-12-04 14:56:52 +0400

    Add comment

--- viceversa/views.py
+++ viceversa/views.py
@@ -6,4 +6,5 @@ def home(requests):
 def reverse(requests):
     user_text = requests.GET['usertext']
     reversed_text = user_text[::-1]
+    # Возвращаем страницу, и 2 переменных исходный текст и перевернутый
     return render(requests, 'reverse.html',{'usertext': user_text, 'reversedtext':reversed_text})
